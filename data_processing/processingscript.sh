434cbccd6f0ac7d0079929529b6a0cd36b24d567
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-08-05 14:43:31 +0100

    calculate_derivatives.py only used in data processing

--- data_processing/processingscript.sh
+++ data_processing/processingscript.sh
@@ -10,7 +10,7 @@ case ${answer:0:1} in
        python step1_REAL_check_data.py CMTSOLUTION_201305240544A_GCMT
        python step2_REAL_flip.py CMTSOLUTION_201305240544A_GCMT
        python automatic_time_windowing_FILTERING_TRIAL.py CMTSOLUTION_201305240544A_GCMT
-       python ../derivatives_calculation/calculate_derivatives.py CMTSOLUTION_201305240544A_GCMT
+       python derivatives_calculation/calculate_derivatives.py CMTSOLUTION_201305240544A_GCMT
        python cut_and_filter_traces.py CMTSOLUTION_201305240544A_GCMT
 
        read -p "Proceed to final check (y/n)? " answer2
