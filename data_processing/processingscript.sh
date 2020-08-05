6f12245f189e49a13f4806b3b998a12dd0f25efb
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-08-05 14:47:50 +0100

    use event_code variable

--- data_processing/processingscript.sh
+++ data_processing/processingscript.sh
@@ -7,16 +7,16 @@ export event_code="CMTSOLUTION_201305240544A_GCMT"
 read -p "Proceed to step 1 (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
-       python step1_REAL_check_data.py CMTSOLUTION_201305240544A_GCMT
-       python step2_REAL_flip.py CMTSOLUTION_201305240544A_GCMT
-       python automatic_time_windowing_FILTERING_TRIAL.py CMTSOLUTION_201305240544A_GCMT
-       python derivatives_calculation/calculate_derivatives.py CMTSOLUTION_201305240544A_GCMT
-       python cut_and_filter_traces.py CMTSOLUTION_201305240544A_GCMT
+       python step1_REAL_check_data.py $event_code
+       python step2_REAL_flip.py $event_code
+       python automatic_time_windowing_FILTERING_TRIAL.py $event_code
+       python derivatives_calculation/calculate_derivatives.py $event_code
+       python cut_and_filter_traces.py $event_code
 
        read -p "Proceed to final check (y/n)? " answer2
        case ${answer:0:1} in 
        y|Y )
-        python final_check.py CMTSOLUTION_201305240544A_GCMT
+        python final_check.py $event_code
        ;;
        * )
         exit 
