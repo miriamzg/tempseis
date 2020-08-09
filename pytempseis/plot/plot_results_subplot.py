281602e174c317a0410b4c04ae9de1ee9e59bcb9
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-08-09 19:19:39 +0100

    deleted commented out lines

--- pytempseis/plot/plot_results_subplot.py
+++ pytempseis/plot/plot_results_subplot.py
@@ -7,42 +7,14 @@ from operator import itemgetter
 import operator
 from matplotlib import rc
 
-# code = "test7.3"
-# inversion_code = "aspect_confined_SHfilt_newlimdat_.500"
-# Event_code = "CMTSOLUTION_200807050212A_GCMT"
-# result_folder = sys.argv[1]
-# result_folder = "/Users/TheStuffofAlice/Dropbox/TEMPSEIS_package_v1.2_WORKING/Inversion/Results/"    + Event_code + "/newdat/" + inversion_code
+
 result_folder = sys.argv[1]
 result_file = result_folder + "/rfi_models"
-# fault_file = "../results/" + code + "/201604151625A_TEST0_info.txt"
 scalar_moment_file = result_folder + "/scalar_moment.txt"
-
-
-# lines = open(fault_file, "r").readlines()
-# phi_t = float(lines[11].split()[1])
-# Amax_t = float(lines[12].split()[1])
-# Amin_t = float(lines[13].split()[1])
-# Duration_t = float(lines[14].split()[1])
-# dip_t =  float(lines[7].split()[1])
-# strike_t =  float(lines[8].split()[1])
-# E_t = Amin_t / Amax_t
-
-
-# Amax_t = 30.
-# Amin_t = 5
-
-# Area_t = Amax_t * Amin_t
-# Duration_t = 7.5
-# phi_t = 0.0
-# v_abs_t = 4.0
-# v_angle_t = 0.0
-# rc_mod_t = 0.0
-# rc_ang_t = 0.0
-# tc_t = 3.25
 
 perc_treshold = 5.0
 
-synt = "n"
+synt = False
 
 mmisfit = []
 AAmax = []
@@ -127,15 +99,6 @@ for i in range(3, len(lines)):
             n += 1
 
 i_best = min(enumerate(mmft), key=itemgetter(1))[0]
-# Amax_best =  AAmax_real[i_best]
-# Amin_best =  AAmin_real[i_best]
-# Phi_best =   pphi[i_best]
-# Duration_best =  dduration_real[i_best]
-# v_abs_best = vv_abs[i_best]
-# v_ang_best = vv_ang[i_best]
-# tc_best = ttc[i_best]
-
-
 min_mft = min(mmft)
 mft_treshold = min_mft * (1 + perc_treshold / 100.0)
 
@@ -278,25 +241,6 @@ print("***")
 rc_mean_module = round(np.median(rrc_mod), 1)
 rc_mean_angle = round(np.median(rrc_ang), 1)
 
-
-# T_mean = round(np.median(dduration_bests),1)
-# T_sd = round(np.std(dduration_bests),1)
-
-# Amax_mean = round(np.median(AAmax_bests),1)
-# Amax_sd = round(np.std(AAmax_bests),1)
-
-# Amin_mean = round(np.median(AAmin_bests),1)
-# Amin_sd = round(np.std(AAmin_bests),1)
-
-# phi_mean = round(np.median(pphi_bests),1)
-# phi_sd = round(np.std(pphi_bests),1)
-
-# v_abs_mean = round(np.mean(vv_abs_bests),1)
-# v_abs_sd = round(np.std(vv_abs_bests),1)
-
-# v_ang_mean = round(np.mean(vv_angle_bests),1)
-# v_ang_sd = round(np.std(vv_angle_bests),1)
-
 out = open(result_folder + "/best_parameters.txt", "w")
 
 out.write("*** threshold: " + str(perc_treshold) + "% ***\n")
@@ -402,25 +346,19 @@ out.close()
 plt.figure(1, figsize=(8.27, 11.69))
 plt.subplot(311)
 plt.scatter(iindex, rrc_mod, color="black", s=2, linewidth=0)
-# plt.axhline(Amax_t, color="green")
 plt.xlim(min(iindex), max(iindex))
-# plt.axhline(rc_mod_t, color="lime", linewidth=2)
 plt.ylabel("Rc module (km)")
 plt.xlabel("\# model")
 
 plt.subplot(312)
 plt.scatter(iindex, rrc_ang, color="black", s=2, linewidth=0)
-# plt.axhline(Amax_t, color="green")
 plt.xlim(min(iindex), max(iindex))
-# plt.axhline(rc_ang_t, color="lime", linewidth=2)
 plt.ylabel("rc angle (deg)")
 plt.xlabel("\# model")
 
 plt.subplot(313)
 plt.scatter(iindex, ttc, color="black", s=2, linewidth=0)
-# plt.axhline(Amax_t, color="green")
 plt.xlim(min(iindex), max(iindex))
-# plt.axhline(tc_t, color="lime", linewidth=2)
 plt.ylabel("tc (s)")
 plt.xlabel("\# model")
 
@@ -433,7 +371,6 @@ plt.subplot(711)
 plt.scatter(iindex, zip(*mmisfit)[0], color="black", s=2, linewidth=0)
 plt.ylabel("Misfit")
 plt.xlabel("\# model")
-# plt.yscale("log")
 plt.xlim(min(iindex), iindex[-1000])
 plt.ylim(0.1 * min(zip(*mmisfit)[0]), 10 * max(zip(*mmisfit)[0]))
 
@@ -444,8 +381,6 @@ plt.axhline(best_Amax, color="0.5", zorder=9)
 plt.axhline(min_Amax, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_Amax, color="0.5", linestyle="--", zorder=9)
 plt.xlim(min(iindex), iindex[-1000])
-# if synt == "y":
-# plt.axhline(Amax_t, color="lime", linewidth=2)
 plt.ylabel("$A_{max}$ (km)")
 plt.xlabel("\# model")
 
@@ -457,8 +392,6 @@ plt.scatter(
 plt.ylabel("$A_{min}$ (km)")
 plt.xlabel("\# model")
 plt.xlim(min(iindex), iindex[-1000])
-# if synt == "y":
-# plt.axhline(Amin_t, color="lime", linewidth=2)
 plt.axhline(best_Amin, color="0.5", zorder=9)
 plt.axhline(min_Amin, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_Amin, color="0.5", linestyle="--", zorder=9)
@@ -468,8 +401,6 @@ plt.scatter(iindex, pphi, color="black", s=2, linewidth=0)
 plt.scatter(iindex_bests, zip(*pphi_bests)[0], color="red", s=2, linewidth=0, zorder=10)
 plt.ylabel("$\Phi$ (deg)")
 plt.xlabel("\# model")
-# if synt == "y":
-# plt.axhline(phi_t, color="lime", linewidth=2)
 plt.axhline(best_phi, color="0.5", zorder=9)
 plt.axhline(min_phi, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_phi, color="0.5", linestyle="--", zorder=9)
@@ -483,8 +414,6 @@ plt.scatter(
 plt.ylabel("$\Delta$ t (s)")
 plt.xlabel("\# model")
 plt.xlim(min(iindex), iindex[-1000])
-# if synt == "y":
-# plt.axhline(Duration_t, color="lime", linewidth=2)
 plt.axhline(best_duration, color="0.5", zorder=9)
 plt.axhline(min_duration, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_duration, color="0.5", linestyle="--", zorder=9)
@@ -497,8 +426,6 @@ plt.scatter(
 plt.ylabel("|v| (km/s)")
 plt.xlabel("\# model")
 plt.xlim(min(iindex), iindex[-1000])
-# if synt == "y":
-# plt.axhline(v_abs_t, color="lime", linewidth=2)
 plt.axhline(best_v_abs, color="0.5", zorder=9)
 plt.axhline(min_v_abs, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_v_abs, linestyle="--", zorder=9)
@@ -511,8 +438,6 @@ plt.scatter(
 plt.ylabel("v angle (deg)")
 plt.xlabel("\# model")
 plt.xlim(min(iindex), iindex[-1000])
-# if synt == "y":
-# plt.axhline(v_angle_t, color="lime", linewidth=2)
 plt.axhline(best_v_ang, color="0.5", zorder=9)
 plt.axhline(min_v_ang, color="0.5", linestyle="--", zorder=9)
 plt.axhline(max_v_ang, color="0.5", linestyle="--", zorder=9)
@@ -525,16 +450,11 @@ plt.figure(1, figsize=(8.27, 11.69))
 font = {"size": 15}
 plt.rc("font", **font)
 
-
 plt.subplot(611)
 plt.subplots_adjust(
     left=None, bottom=0.08, right=None, top=0.98, wspace=0.5, hspace=0.5
 )
 plt.hist(AAmax_real, bins=100, color="C0")
-# plt.axvline(Amax_best, color="red", linewidth=2)
-# plt.axvline(Amax_mean-Amax_sd, color="red", linestyle="--")
-# plt.axvline(Amax_mean+Amax_sd, color="red", linestyle="--")
-# plt.axvline(Amax_t, color="lime", linewidth=2)
 plt.xlabel("$Amax (km$)", size=18)
 plt.ylabel("\# models")
 
@@ -542,34 +462,24 @@ plt.subplot(612)
 plt.hist(AAmin_real, bins=100, color="C1")
 plt.xlabel("Amin (km)", size=18)
 plt.ylabel("\# models")
-# plt.axvline(Amin_best, color="red", linewidth=2)
-# plt.axvline(Amin_mean-Amin_sd, color="red", linestyle="--")
-# plt.axvline(Amin_mean+Amin_sd, color="red", linestyle="--")
-# plt.axvline(Amin_t, color="lime", linewidth=2)
 
 plt.subplot(613)
 plt.hist(pphi, bins=100, color="C2")
-# plt.axvline(Phi_best, color="red", linewidth=2)
-# plt.axvline(phi_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("Phi angle (degrees)", size=18)
 
 plt.subplot(614)
 plt.hist(dduration_real, bins=100, color="C3")
-# plt.axvline(Duration_best, color="red", linewidth=2)
-# plt.axvline(Duration_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("Duration (seconds)", size=18)
 
 plt.subplot(615)
 plt.hist(vv_abs, bins=100, color="C4")
-# plt.axvline(v_abs_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("|v| (km/s)", size=18)
 
 plt.subplot(616)
 plt.hist(vv_ang, bins=100, color="C5")
-# plt.axvline(v_angle_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("v angle (degrees)", size=18)
 
@@ -583,66 +493,44 @@ plt.subplots_adjust(
     left=None, bottom=0.08, right=None, top=0.98, wspace=0.5, hspace=0.5
 )
 plt.hist(AAmax_real, bins=100, color="C0")
-# plt.axvline(Amax_best, color="red", linewidth=2)
-# plt.axvline(Amax_mean-Amax_sd, color="red", linestyle="--")
-# plt.axvline(Amax_mean+Amax_sd, color="red", linestyle="--")
-# plt.axvline(Amax_t, color="lime", linewidth=2)
 plt.xlabel("Amax (km)")
 plt.ylabel("\# models")
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.subplot(612)
 plt.hist(AAmin_real, bins=100, color="C1")
 plt.xlabel("Amin (km)")
 plt.ylabel("\# models")
-# plt.axvline(Amin_best, color="red", linewidth=2)
-# plt.axvline(Amin_mean-Amin_sd, color="red", linestyle="--")
-# plt.axvline(Amin_mean+Amin_sd, color="red", linestyle="--")
-# plt.axvline(Amin_t, color="lime", linewidth=2)
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.subplot(613)
 plt.hist(pphi, bins=100, color="C2")
-# plt.axvline(Phi_best, color="red", linewidth=2)
-# plt.axvline(phi_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("Phi angle (degrees)")
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.subplot(614)
 plt.hist(dduration_real, bins=100, color="C3")
-# plt.axvline(Duration_best, color="red", linewidth=2)
-# plt.axvline(Duration_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("Duration (seconds)")
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.subplot(615)
 plt.hist(vv_abs, bins=100, color="C4")
-# plt.axvline(v_abs_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("|v| (km/s)")
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.subplot(616)
 plt.hist(vv_ang, bins=100, color="C5")
-# plt.axvline(v_angle_t, color="lime", linewidth=2)
 plt.ylabel("\# models")
 plt.xlabel("v angle (degrees)")
 plt.yscale("log")
-# plt.ylim(0,1000)
 
 plt.savefig(result_folder + "/hist2.png")
 plt.close()
 
 # =======================================
-# plt.figure(1, figsize=(11.69, 8.27))
-# plt.subplot(111)
 fig, axs = plt.subplots(4, 2, figsize=(15, 20))
 ax1 = plt.subplot(421)
 plt.subplots_adjust(
@@ -658,22 +546,14 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.axvline(Amax_t, color="green", linewidth=2)
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$A_{max}$ (km)")
 plt.ylabel("Misfit")
 plt.yscale("log")
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "a)", fontweight="bold", transform=ax1.transAxes, size=20)
-# plt.axvline(Amax_t, color="black", linestyle="--", linewidth=3)
-# plt.colorbar(c, orientation="horizontal", label="\# model")
-# plt.suptitle("Amax", fontsize=20)
-# plt.savefig(result_folder+"/Amax_vs_mft.png")
-# plt.close()
 
 ###################
 
-# plt.figure(1, figsize=(11.69, 8.27))
 ax2 = plt.subplot(422)
 plt.subplots_adjust(
     left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.3
@@ -688,19 +568,13 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$A_{min}$ (km)")
 plt.ylabel("Misfit")
 plt.yscale("log")
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "b)", fontweight="bold", transform=ax2.transAxes, size=20)
 
-# plt.suptitle("Amin", fontsize=20)
-# plt.axvline(Amin_t, color="black", linestyle="--", linewidth=3)
-# plt.savefig(result_folder+"/Amin_vs_mft.png")
-# plt.close()
 
-# plt.figure(1, figsize=(11.69, 8.27))
 ax3 = plt.subplot(423)
 plt.subplots_adjust(
     left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.3
@@ -715,18 +589,12 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$\Phi$ ($^\circ$)")
 plt.ylabel("Misfit")
 plt.yscale("log")
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "c)", fontweight="bold", transform=ax3.transAxes, size=20)
-# plt.suptitle("Phi", fontsize=20)
-# plt.axvline(phi_t, color="black", linestyle="--", linewidth=3)
-# plt.savefig(result_folder+"/Phi_vs_mft.png")
-# plt.close()
 
-# plt.figure(1, figsize=(11.69, 8.27))
 ax4 = plt.subplot(424)
 plt.subplots_adjust(
     left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.3
@@ -741,40 +609,13 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$\Delta$ t (s)")
 plt.ylabel("Misfit")
 plt.yscale("log")
-# plt.suptitle("Duration", fontsize=20)
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "d)", fontweight="bold", transform=ax4.transAxes, size=20)
-# plt.axvline(Duration_t, color="black", linestyle="--", linewidth=3)
-# plt.savefig(result_folder+"/Duration_vs_mft.png")
-# plt.close()
-
-# plt.figure(1, figsize=(8.27, 11.69))
-# plt.subplot(211)
-# plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
-# plt.scatter(rrc_mod, zip(*mmisfit)[0], c=nmodel, s=30, vmin=min(nmodel), vmax = 0.1 * max(nmodel), cmap=cm.rainbow, linewidth=0.5)
-# plt.axvline(phi_t, color="green", linewidth=2)
-# plt.xlabel("rc module")
-# plt.ylabel("Misfit")
-# plt.yscale("log")
-# plt.ylim(min(zip(*mmisfit)[0]),max(zip(*mmisfit)[0]))
-
-# plt.subplot(212)
-# plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
-# plt.scatter(rrc_ang, zip(*mmisfit)[0], c=nmodel, s=30, vmin=min(nmodel), vmax = 0.1 * max(nmodel), cmap=cm.rainbow, linewidth=0.5)
-# plt.axvline(phi_t, color="green", linewidth=2)
-# plt.xlabel("rc angle")
-# plt.ylabel("Misfit")
-# plt.yscale("log")
-# plt.ylim(min(zip(*mmisfit)[0]),max(zip(*mmisfit)[0]))
-# plt.savefig(result_folder+"/rc_vs_mft.png")
-# plt.close()
 
 
-# plt.figure(1, figsize=(11.69, 8.27))
 ax5 = plt.subplot(425)
 plt.subplots_adjust(
     left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.3
@@ -789,18 +630,12 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$v_0$ (km s $^{-1}$)")
 plt.ylabel("Misfit")
 plt.yscale("log")
-# plt.suptitle("v module", fontsize=20)
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "e)", fontweight="bold", transform=ax5.transAxes, size=20)
-# plt.axvline(v_abs_t, color="black", linestyle="--", linewidth=3)
-# plt.savefig(result_folder+"/vabs_vs_mft.png")
-# plt.close()
 
-# plt.figure(1, figsize=(11.69, 8.27))
 ax6 = plt.subplot(426)
 plt.subplots_adjust(
     left=None, bottom=None, right=None, top=None, wspace=0.3, hspace=0.3
@@ -815,13 +650,10 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$\Theta$ ($^\circ$)")
 plt.ylabel("Misfit")
 plt.yscale("log")
-# plt.suptitle("V angle", fontsize=20)
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
-# plt.axvline(v_angle_t, color="black", linestyle="--", linewidth=3)
 plt.text(-0.05, 1.05, "f)", fontweight="bold", transform=ax6.transAxes, size=20)
 
 ax6 = plt.subplot(427)
@@ -838,23 +670,17 @@ it = plt.scatter(
     cmap=cm.rainbow,
     linewidth=0.5,
 )
-# plt.colorbar(it, label="iteration number")#, orientation="horizontal")
 plt.xlabel("$\Delta \sigma$ (MPa)")
 plt.ylabel("Misfit")
 plt.yscale("log")
-# plt.suptitle("v module", fontsize=20)
 plt.ylim(min(zip(*mmisfit)[0]), max(zip(*mmisfit)[0]))
 plt.text(-0.05, 1.05, "g)", fontweight="bold", transform=ax6.transAxes, size=20)
-# plt.axvline(v_abs_t, color="black", linestyle="--", linewidth=3)
-# plt.savefig(result_folder+"/stressdrop_vs_mft.png")
-# plt.close()
 
 ax7 = plt.subplot(428)
 ax7.set_visible(False)
 
 cbaxes = fig.add_axes([0.2, 0.03, 0.6, 0.02])
 cb = plt.colorbar(it, cax=cbaxes, label="iteration number", orientation="horizontal")
-# plt.colorbar(it, label="iteration number")
 plt.savefig(result_folder + "/combined.png")
 plt.close()
 
