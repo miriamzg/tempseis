ad81be5d7fe0b24a522a9f5eb0c42c2e44bbd58b
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-10-21 17:41:24 +0100

    remove duplicates in graphic

--- Inversion/NA/src/rfi_subs/rfi_plot/graphic/symbol 3.f
+++
@@ -1,7 +0,0 @@
-
-      subroutine SYMBOL(X,Y,SIZE,IWORD,ANGL,NCHAR)
-      COMMON/P00000/LPLOT,A,B,C,D,ASP,THET
-      COMMON/L00000/PSCA,xo,yo
-      CHARACTER*80 IWORD
-       call xsymbol(x,y,size,iword,angl,nchar,5)
-      end
