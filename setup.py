e2fb3d38b93bba15cc417d505d7089a5368bdd39
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-10-26 10:18:56 +0000

    metadata and requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='pytempseis', version='1.0', packages=find_packages())
+setup(
+    name="pytempseis",
+    version="1.0",
+    author=["Andrea Berbellini", "Alice Turner", "Auggie Marignier"],
+    maintainer_email="augustin.marignier.14@ucl.ac.uk",
+    description="Helpful scripts for processing and plotting higher-order moment tensor inversions",
+    packages=find_packages(),
+    install_requires=["numpy", "obspy", "matplotlib"],
+    extras_require={"dev": ["black", "flake8"]},
+)
